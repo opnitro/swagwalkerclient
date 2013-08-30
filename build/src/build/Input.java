bad83d50ceded95f80d250577e26cf0126979d1e
opnitro <opnitro@gmail.com>
2013-08-30 07:56:45 -0400

    Updated Input.getName();

--- build/src/build/Input.java
+++ build/src/build/Input.java
@@ -9,9 +9,14 @@ private String answer;
 		while(x){
 			System.out.println("Enter user: ");
 			name = scanner.nextLine();
-			System.out.println("Is "+name+" correct");
+			System.out.println("Is "+name+" correct(y/n)");
 			answer = scanner.nextLine();
-			
+			if(answer.equals("y")){
+				x = false;
+			}else{
+				x = true;
+			}
+				
 		}
 		
 		
